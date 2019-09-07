9c861e8334085e26c5a9c95e71ce4881c6242a04
“Chi <bennylin77@gmail.com>
2019-09-07 23:05:38 +0000

    update

--- client/App.js
+++ client/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import { store, history } from './store.js';
-import { hot } from 'react-hot-loader'
+import { hot } from 'react-hot-loader';
 import Layout from 'components/layouts/Layout.js';
+import { actionTypes } from 'actions/authActions';
+const { AUTHENTICATED } = actionTypes
+
+const token = localStorage.getItem('token');
+if(token)
+  store.dispatch({ type: AUTHENTICATED });
 
 
 const App = () => (
